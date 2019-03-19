ee8bd90b2b03bc39474dd4bf5dc731fd18da0fd0
Juan Pablo <34992574+JuanPablo2655@users.noreply.github.com>
2019-03-18 22:00:44 -0400

    fixed the navbar

--- server.js
+++ server.js
@@ -8,7 +8,19 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.render('pages/index');
-})
+});
+
+app.get('/resources', (req, res) => {
+    res.render('pages/resources');
+});
+
+app.get('/about', (req, res) => {
+    res.render('pages/about');
+});
+
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+});
 
 app.listen(port, () => {
     console.log(`cobaltia is listening on port ${port}`);
