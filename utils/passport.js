70f7dca64d31cc3295c172ad54104d6586ac40fb
Juan Pablo <isidroICP@outlook.com>
2019-09-02 20:43:47 -0400

    Update passport.js

--- utils/passport.js
+++ utils/passport.js
@@ -5,7 +5,7 @@ passport.use(new OAuth2Strategy({
     authorizationURL: 'https://discordapp.com/api/oauth2/authorize',
     tokenURL: 'https://discordapp.com/api/oauth2/token',
     clientID: '359869454257094668',
-    clientSecret: '-zpzfp05N18GW4zd6hnH9ZwmL4lfKAi9',
+    clientSecret: 'e',
     callbackURL: '/auth/discord/redirect'
 }, (accessToken, refreshToken, profile, done) => {
     console.log('epic it worked');
